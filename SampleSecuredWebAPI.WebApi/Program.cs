e191afa1614fb9fc53ec309811be67e455239c28
sibille <sibille@ilitia.com>
2020-05-27 11:44:11 +0200

    Update to .NET Core 3.1

--- SampleSecuredWebAPI.WebApi/Program.cs
+++ SampleSecuredWebAPI.WebApi/Program.cs
@@ -5,6 +5,7 @@ using System.IO;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace SampleSecuredWebAPI.WebApi
 {
@@ -12,11 +13,14 @@ namespace SampleSecuredWebAPI.WebApi
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+           Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
     }
 }
--- SampleSecuredWebAPI.WebApi/Startup.cs
+++ SampleSecuredWebAPI.WebApi/Startup.cs
@@ -3,7 +3,8 @@ using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
@@ -24,8 +25,10 @@ namespace SampleSecuredWebAPI.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.ProtectWebApiWithJwtBearer(Configuration);
+
+            services.AddControllers();
+
             services.AddSingleton<IItemRepository, ItemRepository>();
 
             services.AddSwaggerGen(c =>
@@ -34,20 +37,22 @@ namespace SampleSecuredWebAPI.WebApi
             });
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseHsts();
-            }
 
             app.UseHttpsRedirection();
+            app.UseRouting();
+
             app.UseAuthentication();
-            app.UseMvc();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+            });
 
             app.UseSwagger();
             app.UseSwaggerUI(c =>
